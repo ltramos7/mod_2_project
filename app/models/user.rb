b1cc8290303232b3c0ca1210e9b7de55ed009898
John Leavell <johnleavell@gmail.com>
2020-02-11 13:55:13 -0600

    updated assoications and migrated

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
     has_many :favorite_murals
     has_many :murals, through: :favorite_murals
     
+    has_many :artist_favorites
+    has_many :artist, through: :artist_favorites
     
 end
--- db/migrate/20200211173114_create_murals.rb
+++ db/migrate/20200211173114_create_murals.rb
@@ -1,10 +1,10 @@
 class CreateMurals < ActiveRecord::Migration[6.0]
   def change
     create_table :murals do |t|
-      t.text :mural_name
+      t.string :mural_name
       t.integer :artist_id
       t.integer :commissinor_id
-      t.text :location
+      t.string :location
 
       t.timestamps
     end
