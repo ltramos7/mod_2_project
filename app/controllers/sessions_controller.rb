1ad80378fe611c6d3a4822f6a5d3e426ee1c5563
Linda <ltramos7@gmail.com>
2020-02-12 18:02:33 -0600

    adding methods in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    # session[:user_name] = params[:user_name]
     session[:user_name] = params[:user_name]
-    redirect_to '/users/id'
+    redirect_to '/users'
+  end
+
+  def destroy
+    session.delete(:id)
+    redirect_to login_path
   end
 
 end
