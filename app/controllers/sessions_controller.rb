9644dae70b65551287cfd0be0b0443198c20ae0e
John Leavell <johnleavell@gmail.com>
2020-02-12 18:02:53 -0600

    added user login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(user_name: params[:user_name])
+    if user 
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      flash[:message] = "Invalid credentials"
+      redirect_to login_path
+    end
+  end
 
-    session[:user_name] = params[:user_name]
-    redirect_to '/users/id'
+  def destroy
+    p "LOGGING OUT"
+    session[:user_id] = nil
+    # session.delete(:user_id)
+    redirect_to login_path
   end
 
 end
